e9727ab863b399ce94100ef8d227bff79eca5336
StrategFirst <mathieu.toulon@free.fr>
2023-05-09 20:31:42 +0200

    📈 Follow web standard
    Bing started follow common web standard such has using meta tags, and webp instead of jpeg
    Also making their architecture cleaner
    Even if some artifact remains
    Those changes must be followed in our process

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { parse } from 'node-html-parser'
 
 // Config
 import COUNTRIES from '/etc/scrapconfig/countries.json' assert { type: 'json' }
-import URIS from '/etc/scrapconfig/uris.json' assert { type: 'json' }
 
 // Usefull function
 async function fileFromURL( webURL , localPath ) {
@@ -15,23 +14,43 @@ async function fileFromURL( webURL , localPath ) {
 }
 
 // Main
-for(const country of COUNTRIES ) {
+async function TodayMetadata() {
+	return (
+			( await Promise.all(
+				COUNTRIES.map(
+					country => fetch( `https://www.bing.com?cc=${country.code}` )	
+						.then( res => res.text() )					
+						.then( txt => parse(txt) )					
+						.then( dom => dom.querySelectorAll('meta') )
+						.then( lst => lst.map( tag => [ tag.getAttribute('property') , tag.getAttribute('content') ] ) )
+						.then( ent => Object.fromEntries(ent) )
+						.then( obj => [ obj["og:image"] , obj["og:description"] , country.name ] )
+				)
+			) )
+		
+			.filter( x => x.every( k => k != undefined ) )
+			.map( ([url,desc,country]) => ({
+					country, desc,
+					'url' : url.replace('_tmb.jpg&rf=','_1920x1080.webp&qlt=100'),
+					'file' : url.match(/OHR\.(.*)_([^_]+)_tmb/)[1]
+			}) )
 	
-	fetch( `${URIS.source}?cc=${country.code}` )	// Page à scrapper
-		.then( response => response.text() )		// Récupération de l'HTML
-		.then( text => parse(text) ) 				// Parsing de la page HTML
-		.then( result => URIS.dom
-							.map( ({path}) => result.querySelector(path) ) 				// Recherche des balises souhaités
-							.filter( element => (element != null ) ) 					// Retrait des recherches échoués
-							.map( tag => `${URIS.source}${tag.getAttribute('href')}` )	// Génération de l'URL complète
-							.map( data => data.replace(/1920x1200/g,'1920x1080') )		// Tricks : l'image est dispo avec une résolution de 1920x1080 et cette résolution n'as pas de watermark ^^
-							.filter( element => (element != null ) ) 					// Retrait des recherches échoués (au cas où)
-							.forEach( imageURL => {
-								let source = imageURL
-								let destination = (new URL(imageURL)).searchParams.get('id');	// Le nom est dans l'URL mais attention il reste un artifact d'un truc mal fait chez eux 
-								if( destination == null ) return; 								// Sécurité de plus
-								destination = `${URIS.destination}${destination.replace(/OHR.([^_]+).*jpg/,(_,m)=>`${m}.jpg`)}`	// Nettoyage du nom du fichier pour récupérer le prefix et suffix de bing qui pour nous est inutile et lourd
-								fileFromURL( source , destination ) 							// Téléchargement
-							} )
-			)
+	)
 }
+
+async function TodayData( metadata ) {
+
+	// Download only once
+	await Promise.all(
+		Object.entries(
+			Object.fromEntries(
+				metadata.map( md => [md.file,md.url] )
+			)
+		)
+		.map( ([file,url]) => fileFromURL(url,`/var/resources/${file}.webp`) )
+	)
+
+	console.table( metadata )
+
+}
+TodayMetadata().then( TodayData )
