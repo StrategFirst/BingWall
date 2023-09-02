04ddb3c5de9d59cc5b06960f8a9577066800a9df
StrategFirst <mathieu.toulon@free.fr>
2023-09-02 23:08:48 +0200

    📦 Delivery packages monthly !

--- pack-up.sh
+++ pack-up.sh
@@ -9,7 +9,7 @@ artifactURLs=$(\
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $GITHUB_TOKEN" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/StrategFirst/BingWall/actions/artifacts?name=TodaysPicture \
+    https://api.github.com/repos/StrategFirst/BingWall/actions/artifacts?name=DailyPictures \
   | grep archive_download_url \
   | grep -E -o 'https:[^"]+/zip'
   )
