4067ccc2477659c94a29673464515929ff0a1940
StrategFirst <mathieu.toulon@free.fr>
2023-09-09 22:29:04 +0200

    📈 Feature : Update timer

---
+++ web/scripts/clock.js
@@ -0,0 +1,23 @@
+setInterval( () => {
+
+	const delta = (1000 * 60 * 60 * 24) - ( new Date() - new Date(document.lastModified) ) ;
+
+	
+	const domTarget = document.querySelector('#countdown')
+
+	if( delta < 0 ) {
+
+		domTarget.innerText = `Rafraichir`
+		domTarget.onclick = () => document.location.reload()
+		domTarget.classList.add('expired')
+		domTarget.style.setProperty( '--jauge-state' , 1 )
+
+	} else {
+		const delayText = `${(~~(delta/3600000))%60}h ${(~~(delta/60000))%60}m ${(~~(delta/1000))%60}s`
+		const delayRatio = delta / (1000 * 60 * 60 * 24)
+
+		domTarget.innerText = delayText
+		domTarget.style.setProperty( '--jauge-state' , (~~(delayRatio*1e5))/1e5 )
+	}
+
+}, 2000)
