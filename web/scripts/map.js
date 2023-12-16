005396435d3eacfd860dd83821a6333a3182af43
StrategFirst <mathieu.toulon@free.fr>
2023-12-16 22:51:06 +0100

    Swap map marker

--- web/scripts/map.js
+++ web/scripts/map.js
@@ -5,6 +5,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+var markerIcon = L.icon({
+    iconUrl: 'assets/camera.svg',
+})
+
 /**
  * Add a marker to the page map with the given information
  * and return an html anchor to it
@@ -16,7 +20,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
  * @returns {string} A dom string storing the HTML with a anchor link to the generated marker
  */
 function addMarker(lat, long, title, img_path) {
-    var marker = L.marker([lat, long]).addTo(map);
+    var marker = L.marker([lat, long],{icon:markerIcon}).addTo(map);
     let anchor_name = `map_marker${title.replace(/[^a-zA-Z0-9_-]/ig,'')}`
     marker.bindPopup(`<strong id="${anchor_name}">${title}</strong> <img class="map_img_miniature" src="${img_path}" />`);
     return `<a href="#${anchor_name}" class="map-marker-link"> Emplacement sur la carte. </a>`;
