56ebc5019bc8464b918c445c4da71b165b1a77cd
StrategFirst <mathieu.toulon@free.fr>
2023-12-22 23:58:03 +0100

    💽 Make buffer for icon to optimise query

--- web/scripts/icon.js
+++ web/scripts/icon.js
@@ -1,9 +1,26 @@
 function updateLocalIcon() {
+
+	async function getIcon(key) {
+		const lsk = `svg-icon.${key}`;
+
+		// Try to use localStorage as buffer cache
+		let fromBuffer = localStorage.getItem(lsk);
+		if( fromBuffer != null ) {
+			return fromBuffer;
+		}
+
+		// Normal load and save in buffer cache
+		let res = await fetch(`./assets/${key}.svg`);
+		let txt = await res.text();
+		localStorage.setItem(lsk, txt);
+		return txt;
+	}
+
+
 	([... document.querySelectorAll(':not(.local-icon-loaded).local-icon')])
-		.forEach( (srcElem) => fetch(`./assets/${srcElem.innerText}.svg`)
-								.then( res => res.text() )
-								.then( txt => srcElem.parentNode.innerHTML = txt )
-								.then( ___ => srcElem.classList.add('local-icon-loaded'))
+		.forEach( (srcElem) => getIcon(srcElem.innerText)
+						.then( txt => srcElem.parentNode.innerHTML = txt )
+						.then( ___ => srcElem.classList.add('local-icon-loaded'))
 		)
 }
 
